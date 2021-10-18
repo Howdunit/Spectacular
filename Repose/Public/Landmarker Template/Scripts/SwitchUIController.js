cf305068797fecf3abde340f0520e90f699cfd3a
howardwon <howard.hj.won@gmail.com>
2021-10-17 21:12:04 -0400

    Repose Landmark PoC

---
+++ Repose/Public/Landmarker Template/Scripts/SwitchUIController.js
@@ -0,0 +1,37 @@
+// SwitchModeController.js
+// Version: 0.0.2
+// Event: Lens Initialized
+// Description: Controls the debug control UI
+
+// @input bool Advanced
+
+// @ui {"widget":"group_start","label":"References", "showIf": "Advanced"}
+// @input Component.ScriptComponent landmarkerController
+
+// @input SceneObject birdsEyeVisual
+// @input SceneObject fullSizeVisual
+
+// @ui {"widget":"group_end"}
+
+script.api.setToggleVisual = setToggleVisual;
+
+// Events
+function onTouchStart(eventData) {
+    if (
+        script.landmarkerController &&
+        script.landmarkerController.api.toggleMarkerMode
+    ) {
+        var currentMode = script.landmarkerController.api.toggleMarkerMode();
+        setToggleVisual(currentMode);
+    }
+}
+
+// Helpers
+function setToggleVisual(currentMode) {
+    var birdsEyeEnabled = currentMode === "BirdsEye";
+    script.birdsEyeVisual.enabled = birdsEyeEnabled;
+    script.fullSizeVisual.enabled = !birdsEyeEnabled;
+}
+
+var touchStartEvent = script.createEvent("TouchStartEvent");
+touchStartEvent.bind(onTouchStart);
